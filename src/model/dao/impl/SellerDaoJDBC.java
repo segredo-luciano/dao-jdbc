e6964eff9a89fc18ec66d428f93d21bfe9440cbd
segredo-luciano <lucianosegredo20@gmail.com>
2023-10-04 16:02:19 -0300

    implementing find by id

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	private Connection connection;
+	
+	public SellerDaoJDBC(Connection connection) {
+		this.connection = connection;
+	}
+	
 	@Override
 	public void insert(Seller department) {		
 		
@@ -26,8 +39,41 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		try {
+			preparedStatement = connection.prepareStatement(
+					"select seller.*, department.Name as depName"
+					+ " from seller inner join department on seller.Id = ?"
+					);
+			
+			preparedStatement.setInt(1, id);
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next()) {
+				Department department = new Department();
+				department.setId(resultSet.getInt("DepartmentId"));
+				department.setName(resultSet.getString("DepName"));
+				
+				Seller seller = new Seller(
+					resultSet.getInt("Id"),
+					resultSet.getString("Name"),
+					resultSet.getString("Email"),
+					resultSet.getDate("BirthDate"),
+					resultSet.getDouble("BaseSalary"),
+					department
+				);
+				
+				return seller;
+			}
+			
+			return null;
+		} catch(SQLException sqle) {
+			throw new DbException(sqle.getMessage());
+		} finally {
+			DB.closeStatement(preparedStatement);
+			DB.closeResultSet(resultSet);
+		}
 	}
 
 	@Override
